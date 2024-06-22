c83fe3eeebd72b4ba45883b4194ecefe13398e20
2immons <podoprigoraps@gmail.com>
2024-06-22 21:52:27 +0400

    Парсинг всех страниц

--- src/main/java/com/sar/psapp/service/buttonsHandlers/ButtonHandlerImpl.java
+++ src/main/java/com/sar/psapp/service/buttonsHandlers/ButtonHandlerImpl.java
@@ -4,25 +4,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 @Component
 public class ButtonHandlerImpl implements ButtonHandler {
 
     @Override
-    public void handleAgeButton(WebDriver driver) {
+    public void handleAllButtons(WebDriver driver, int waitTime, int pageLimit) {
+        this.handleAgeButton(driver, waitTime);
+        this.handleRegionButton(driver, waitTime);
+    }
+
+    @Override
+    public void handleAgeButton(WebDriver driver, int waitTime) {
         try {
-            WebElement ageButton = driver.findElement(By.className("adult-disclaimer__btn"));
-            ageButton.click();
+            Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.className("adult-disclaimer__btn")));
+            button.click();
         } catch (Exception e) {
             System.out.println("Кнопка возраста не найдена");
         }
     }
 
     @Override
-    public void handleRegionButton(WebDriver driver) {
+    public void handleRegionButton(WebDriver driver, int waitTime) {
         try {
-            WebElement submitButton = driver.findElement(By.className("header-region-selector-view__footer-ok"));
-            submitButton.click();
+            Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.className("header-region-selector-view__footer-ok")));
+            button.click();
         }
         catch (Exception e) {
             System.out.println("Кнопка региона не найдена");
