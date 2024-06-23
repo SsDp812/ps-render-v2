d493c91b6fb944ff8d1baa05aac69683ce0f09cd
2immons <podoprigoraps@gmail.com>
2024-06-23 04:48:57 +0400

    добавлена многопоточность через создание новых WebDriver с куками базового

--- src/main/java/com/sar/psapp/service/ParserService.java
+++ src/main/java/com/sar/psapp/service/ParserService.java
@@ -3,10 +3,14 @@ package com.sar.psapp.service;
 import com.sar.psapp.dto.Card;
 import com.sar.psapp.dto.StartProcess;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
 public interface ParserService {
     List<Card> parseBySettings(StartProcess settings);
+    WebDriver initializeBaseDriver(List<Card> cardsList, StartProcess settings);
+    void parsePages(List<Card> cardsList, int pageLimit, WebDriver baseDriver, StartProcess settings);
+    WebDriver createNewWebDriver(WebDriver baseDriver, String taskUrl);
     List<Card> parsePage(WebDriver driver, StartProcess settings);
 }
--- src/main/java/com/sar/psapp/service/impl/MegaMarketImpl.java
+++ src/main/java/com/sar/psapp/service/impl/MegaMarketImpl.java
@@ -6,22 +6,21 @@ import com.sar.psapp.service.ParserService;
 import com.sar.psapp.service.buttonsHandlers.ButtonHandler;
 import com.sar.psapp.service.fileHandler.FileHandler;
 import com.sar.psapp.service.validation.GoodsValidator;
-import org.checkerframework.checker.units.qual.A;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.*;
 
 @Component
 public class MegaMarketImpl implements ParserService {
@@ -46,23 +45,87 @@ public class MegaMarketImpl implements ParserService {
 
         List<Card> cardsList = new ArrayList<>();
 
-        WebDriver driver = new ChromeDriver();
-        String url = settings.getCategory().getMegaMarketUrl();
+        Long start = System.currentTimeMillis();
 
-        for (int pageNumber = 1; pageNumber <= pageLimit; pageNumber++) {
-            if (pageNumber > 1) {
-                url = settings.getCategory().getMegaMarketUrl() + "page-" + pageNumber + "/";
-            }
-            driver.get(url);
-            System.out.println("URL текущей страницы: " + driver.getCurrentUrl());
-            cardsList.addAll(this.parsePage(driver, settings));
-        }
+        // Создание базового WebDriver и парсинг первой страницы для сохранения куков
+        WebDriver baseDriver = initializeBaseDriver(cardsList, settings);
+        // Многопоточный парсинг остальных страниц, используя куки базового WebDriver
+        parsePages(cardsList, pageLimit, baseDriver, settings);
+        baseDriver.quit();
 
-        driver.quit();
+        Long stop = System.currentTimeMillis();
+        System.out.println(stop - start);
 
         fileHandler.csvFileHandler(cardsList);
 
         return cardsList;
+    }
+
+    // initializeBaseDriver создает базовый WebDriver и парсит первую страницу для сохранения куков
+    @Override
+    public WebDriver initializeBaseDriver(List<Card> cardsList, StartProcess settings) {
+        WebDriver baseDriver = new ChromeDriver();
+
+        String url = settings.getCategory().getMegaMarketUrl();
+        baseDriver.get(url);
+        System.out.println("URL текущей страницы: " + baseDriver.getCurrentUrl());
+
+        cardsList.addAll(this.parsePage(baseDriver, settings));
+
+        return baseDriver;
+    }
+
+    // parsePages запускает многопоточный парсинг страниц
+    @Override
+    public void parsePages(List<Card> cardsList, int pageLimit, WebDriver baseDriver, StartProcess settings) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<List<Card>>> futures = new ArrayList<>();
+
+        for (int pageNumber = 2; pageNumber <= pageLimit; pageNumber++) {
+            int taskPageNumber = pageNumber;
+
+            Callable<List<Card>> task = () -> {
+                String taskUrl = settings.getCategory().getMegaMarketUrl() + "page-" + taskPageNumber + "/";
+
+                // Создание нового WebDriver с куками базового
+                WebDriver taskDriver = createNewWebDriver(baseDriver, taskUrl);
+
+                // Парсинг страницы с помощью созданного WebDriver
+                List<Card> cards = parsePage(taskDriver, settings);
+                taskDriver.quit();
+
+                return cards;
+            };
+
+            futures.add(executorService.submit(task));
+        }
+
+        for (Future<List<Card>> future : futures) {
+            try {
+                cardsList.addAll(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executorService.shutdown();
+    }
+
+    // createNewWebDriver возвращает новый WebDriver, копируя в него куки базового драйвера
+    @Override
+    public WebDriver createNewWebDriver(WebDriver baseDriver, String taskUrl) {
+        WebDriver taskDriver = new ChromeDriver();
+
+        taskDriver.get(taskUrl);
+        System.out.println("URL текущей страницы: " + taskDriver.getCurrentUrl());
+
+        for (org.openqa.selenium.Cookie cookie : baseDriver.manage().getCookies()) {
+            taskDriver.manage().addCookie(cookie);
+        }
+
+        taskDriver.navigate().refresh();
+
+        return taskDriver;
     }
 
     // parsePage парсит страницу
@@ -76,7 +139,6 @@ public class MegaMarketImpl implements ParserService {
         }
 
         List<Card> cardsResponse = new ArrayList<>();
-        Long start = System.currentTimeMillis();
         Document doc = Jsoup.parse(driver.getPageSource());
         Elements elements = doc.select(".catalog-item-mobile");
         for (Element card : elements) {
@@ -91,8 +153,8 @@ public class MegaMarketImpl implements ParserService {
             Elements price = card.select(".item-price");
             String textPrice = price.get(0).text().substring(0, price.get(0).text().length() - 2);
             StringBuilder textPriceFormatted = new StringBuilder();
-            for(char ch : textPrice.toCharArray()) {
-                if(ch != ' '){
+            for (char ch : textPrice.toCharArray()) {
+                if (ch != ' ') {
                     textPriceFormatted.append(ch);
                 }
             }
@@ -104,13 +166,11 @@ public class MegaMarketImpl implements ParserService {
             String url = link.attr("href");
             cardResponse.setUrl(MEGA_MARKET_BASE_URL + url);
 
-            if(goodsValidator.validateForBenefit(cardResponse,settings)){
+            if (goodsValidator.validateForBenefit(cardResponse, settings)) {
                 cardsResponse.add(cardResponse);
             }
 
         }
-        Long stop = System.currentTimeMillis();
-        System.out.println(stop - start);
         return cardsResponse;
     }
 }
